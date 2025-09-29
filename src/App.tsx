8f506654d2f2b111d2d97e1733a614bed57394ab
marcelogomesdc <adsmarcelogomes@gmail.com>
2025-09-28 23:18:35 -0300

    [FIX] - :hammer: Ajuste de erro de build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Hero from './components/Hero';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Portfolio from '../components/Portfolio';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 const App: React.FC = () => {
     return (
